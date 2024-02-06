a417c9d71855f5d738f6daba4e51d8d3373d5a7e
User_ananas <nastaskachenko02@gmail.com>
2024-02-06 23:23:43 +0100

    laser special packages are ready

--- static/scripts/laser-new.js
+++ static/scripts/laser-new.js
@@ -17,9 +17,28 @@ function changePrices(selectElement) {
 
 function displayVector(element) {
   let id = element.id
+  let vectors = document.querySelectorAll('.vector') 
+  vectors.forEach(function(vector) {vector.style.visibility = 'hidden'})
+  let prices = document.querySelectorAll('.price')
+  prices.forEach(function(price) {
+    if (price.classList.contains('choosed')) {
+      price.classList.remove('choosed')
+    }
+  })
+
+  element.classList.add('choosed')
+  
   if (id == 'price-partleg') {
     div = document.getElementById('vector-partleg');
+  } if (id == 'price-bikini') { 
+    div = document.getElementById('vector-bikini'); 
+  } if (id == 'price-legs') { 
+    div = document.getElementById('vector-legs')
+  } if (id == 'price-partleg-bikini') {
+    div = document.getElementById('vector-partleg-bikini')
+  } if (id == 'price-leg-bikini') {
+    div = document.getElementById('vector-leg-bikini')
+  }
   div.style.visibility = "visible";
-}
   }
 
