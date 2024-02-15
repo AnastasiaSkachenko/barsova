b4002730dd11c5e387e405c027b9ce6c7cf8b397
User_ananas <nastaskachenko02@gmail.com>
2024-02-15 22:19:27 +0100

    functionality for face and armpits is ready

--- static/scripts/laser1.js
+++ static/scripts/laser1.js
@@ -42,3 +42,50 @@ function displayVector(element) {
   div.style.visibility = "visible";
   }
 
+function displayDesctiption(element) { 
+  let allDescriptions = document.querySelectorAll('.description-parse')
+  console.log(allDescriptions)
+  allDescriptions.forEach(function(description) { 
+    description.style.visibility = 'hidden'
+     
+  })
+ 
+  
+  if (element.id == 'armpit-left' || element.id == 'armpit-right') {
+  document.getElementById('armpits-description').style.visibility = 'visible'
+  } else if (element.id == 'face-area') { 
+    document.getElementById('face-description').style.visibility = 'visible'
+  }
+}
+
+function showTip(element) {
+  element.style.cursor = 'pointer';
+  if (element.id == 'armpit-left') {
+    let tip = document.getElementById('armpit')
+    tip.style.right = '190px';
+    tip.style.top = '150px';
+    tip.style.visibility = 'visible' 
+  } else if (element.id == 'armpit-right') {
+    let tip = document.getElementById('armpit')
+    tip.style.right = '125px';
+    tip.style.top = '150px';
+    tip.style.visibility = 'visible' 
+  } else if (element.id == 'face-area') {
+    let tip = document.getElementById('face')
+    tip.style.visibility = 'visible';
+    tip.style.right = '125px';
+    tip.style.top = '30px'
+  }
+}
+
+function hideTip(element) {
+  let tip
+  if (element.id == 'armpit-left' || element.id == 'armpit-right') {
+    tip = document.getElementById('armpit')
+    
+  } else if (element.id == 'face-area') {
+    tip = document.getElementById('face') 
+  }
+  tip.style.visibility = 'hidden'
+}
+ 
