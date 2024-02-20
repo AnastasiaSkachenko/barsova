440663ac5cc2c5f15ca62b48cbc65a8573744bdd
User_ananas <nastaskachenko02@gmail.com>
2024-02-20 21:33:30 +0100

    image map done

--- static/scripts/laser2.js
+++ static/scripts/laser2.js
@@ -96,16 +96,16 @@ function showTip(element) {
     tip.style.right = '150px'
   } else if (element.id == 'belly') {
     tip= document.getElementById('belly-tip') 
-    tip.style.top = '190px'
+    tip.style.top = '180px'
     tip.style.right = '135px'
   } else if (element.id == 'left-hand') {
     tip= document.getElementById('hands-tip') 
-    tip.style.top = '190px'
-    tip.style.right = '150px'
+    tip.style.top = '155px'
+    tip.style.right = '185px'
   } else if (element.id == 'right-hand') {
     tip= document.getElementById('hands-tip') 
-    tip.style.top = '125px'
-    tip.style.right = '150px'
+    tip.style.top = '155px'
+    tip.style.right = '110px'
   } else if (element.id == 'legs-area') {
     tip= document.getElementById('legs-tip') 
     tip.style.top = '235px'
