a0d22ce68c4b03ec93109ef5dbf1cc24001b7faf
User_ananas <nastaskachenko02@gmail.com>
2024-05-24 01:43:36 +0200

    done adaptations for laser

--- static/scripts/laser_c.js
+++ static/scripts/laser_c.js
@@ -76,70 +76,81 @@ function displayDesctiption(element) {
 }
 
 function showTip(element) {
+  const width = window.innerWidth;
   element.style.cursor = 'pointer';
-  let tip
-  if (element.id == 'armpit-left') {
-    tip = document.getElementById('armpit')
-    tip.style.right = '190px';
-    tip.style.top = '150px'; 
-  } else if (element.id == 'armpit-right') {
-    tip = document.getElementById('armpit')
-    tip.style.right = '125px';
-    tip.style.top = '150px'; 
-  } else if (element.id == 'face-area') {
-    tip = document.getElementById('face') 
-    tip.style.right = '125px';
-    tip.style.top = '30px'
-  } else if (element.id == 'chest') {
-    tip = document.getElementById('chest-tip')
-    tip.style.right = '140px';
-    tip.style.top = '140px'
-  } else if (element.id == 'bikini') {
-    tip= document.getElementById('bikini-tip') 
-    tip.style.top = '225px'
-    tip.style.right = '150px'
-  } else if (element.id == 'belly') {
-    tip= document.getElementById('belly-tip') 
-    tip.style.top = '180px'
-    tip.style.right = '135px'
-  } else if (element.id == 'left-hand') {
-    tip= document.getElementById('hands-tip') 
-    tip.style.top = '155px'
-    tip.style.right = '185px'
-  } else if (element.id == 'right-hand') {
-    tip= document.getElementById('hands-tip') 
-    tip.style.top = '155px'
-    tip.style.right = '110px'
-  } else if (element.id == 'legs-area') {
-    tip= document.getElementById('legs-tip') 
-    tip.style.top = '235px'
-    tip.style.right = '140px'
-  }
+  let tip;
 
-  tip.style.visibility = 'visible'
+  const tipsLargeScreen = {
+    'armpit-left': { id: 'armpit', right: '190px', top: '150px' },
+    'armpit-right': { id: 'armpit', right: '125px', top: '150px' },
+    'face-area': { id: 'face', right: '125px', top: '30px' },
+    'chest': { id: 'chest-tip', right: '140px', top: '140px' },
+    'bikini': { id: 'bikini-tip', right: '150px', top: '225px' },
+    'belly': { id: 'belly-tip', right: '135px', top: '180px' },
+    'left-hand': { id: 'hands-tip', right: '185px', top: '155px' },
+    'right-hand': { id: 'hands-tip', right: '110px', top: '155px' },
+    'legs-area': { id: 'legs-tip', right: '140px', top: '235px' }
+  };
+
+  const tipsSmallScreen = {
+    'armpit-left': { id: 'armpits-description', right: '170px', top: '200px' },
+    'armpit-right': { id: 'armpits-description', right: '170px', top: '200px' },
+    'face-area': { id: 'face-description', right: '170px', top: '120px' },
+    'chest': { id: 'chest-description', right: '140px', top: '200px' },
+    'bikini': { id: 'bikini-description', right: '150px', top: '280px' },
+    'belly': { id: 'belly-description', right: '135px', top: '260px' },
+    'left-hand': { id: 'hands-description', right: '185px', top: '250px' },
+    'right-hand': { id: 'hands-description', right: '185px', top: '250px' },
+    'legs-area': { id: 'legs-description', right: '140px', top: '100px' }
+  };
+
+  const tips = width > 1050 ? tipsLargeScreen : tipsSmallScreen;
+  const tipInfo = tips[element.id];
+
+  if (tipInfo) {
+    tip = document.getElementById(tipInfo.id);
+    tip.style.right = tipInfo.right;
+    tip.style.top = tipInfo.top;
+    tip.style.visibility = 'visible';
+  }
 }
 
 function hideTip(element) {
-  let tip
-  if (element.id == 'armpit-left' || element.id == 'armpit-right') {
-    tip = document.getElementById('armpit')
-  } else if (element.id == 'face-area') {
-    tip = document.getElementById('face') 
-  } else if (element.id == 'chest') {
-    tip = document.getElementById('chest-tip')
-  } else if (element.id == 'bikini') {
-    tip = document.getElementById('bikini-tip')
-  } else if (element.id == 'belly') {
-    tip = document.getElementById('belly-tip')
-  } else if (element.id == 'left-hand' || element.id == 'right-hand') {
-    tip = document.getElementById('hands-tip')
-  } else if (element.id == 'legs-area') {
-    tip = document.getElementById('legs-tip') 
-  }  
+  const width = window.innerWidth;
+  let tip;
 
-  tip.style.visibility = 'hidden'
+  const tipsLargeScreen = {
+    'armpit-left': 'armpit',
+    'armpit-right': 'armpit',
+    'face-area': 'face',
+    'chest': 'chest-tip',
+    'bikini': 'bikini-tip',
+    'belly': 'belly-tip',
+    'left-hand': 'hands-tip',
+    'right-hand': 'hands-tip',
+    'legs-area': 'legs-tip'
+  };
+
+  const tipsSmallScreen = {
+    'armpit-left': 'armpits-description',
+    'armpit-right': 'armpits-description',
+    'face-area': 'face-description',
+    'chest': 'chest-description',
+    'bikini': 'bikini-description',
+    'belly': 'belly-description',
+    'left-hand': 'hands-description',
+    'right-hand': 'hands-description',
+    'legs-area': 'legs-description'
+  };
+
+  const tips = width > 1050 ? tipsLargeScreen : tipsSmallScreen;
+  const tipId = tips[element.id];
+
+  if (tipId) {
+    tip = document.getElementById(tipId);
+    tip.style.visibility = 'hidden';
+  }
 }
-
 
 let beforeAfterLaserAll = ``
 
@@ -236,3 +247,40 @@ for (let i=10; i<13; i++){
 document.querySelector('.contraindication-hard').innerHTML = contraindicationsHard
 document.querySelector('.contraindication-light').innerHTML = contraindicationsLight
 
+
+function showDescription(element) {
+  
+}
+
+
+
+
+function adjuctContent() {
+  let width = window.innerWidth
+  let map = window.getElementById('map-body')
+  if (width > 1000) {
+    map.innerHTML = ```<area class="body-part area" shape="circle" coords="165, 170, 13"  alt="armpit-left" id="armpit-left" onmouseover="showTip(this)" onclick="displayDesctiption(this)" onmouseleave="hideTip(this)">
+    <area class="body-part" shape="circle" coords="220, 170, 13"  alt="armpit-right" id="armpit-right" onmouseover="showTip(this)" onmouseleave="hideTip(this)" onclick="displayDesctiption(this)">
+    <area class="body-part"  shape="poly" coords="150, 40, 185, 30, 210, 25, 230, 70, 240, 95, 250, 105, 240, 110, 180, 115, 170, 110 " alt="face" id="face-area" onclick="displayDesctiption(this)" onmouseover="showTip(this)" onmouseleave="hideTip(this)">
+    <area class="body-part" shape="rect" coords="160, 155, 205, 190" alt="chest" id="chest" onmouseover="showTip(this)" onmouseleave="hideTip(this)" onclick="displayDesctiption(this)">
+    <area class="body-part" shape="rect" coords="175, 245, 205, 275" alt="bikini" id="bikini" onmouseover="showTip(this)" onmouseleave="hideTip(this)" onclick="displayDesctiption(this)">
+    
+    <area class="body-part" shape="poly" coords="170, 185, 215, 185, 223, 200, 230, 245, 155, 245,  165, 200" alt="belly" id="belly" onmouseover="showTip(this)" onmouseleave="hideTip(this)" onclick="displayDesctiption(this)">
+    <area class="body-part" shape="poly" coords="140, 160, 160, 180, 160, 205, 150, 245, 155, 260, 150, 265, 135, 240, 150, 295, 133, 260, 135, 245, 138, 205" alt="left-hand" id="left-hand" onmouseover="showTip(this)" onmouseleave="hideTip(this)" onclick="displayDesctiption(this)">                        
+    <area class="body-part" shape="poly" coords="220, 180, 240, 160, 252, 205, 254, 240, 262, 245,   245, 290, 223, 255, 233, 240, 226, 210" alt="right-hand", id="right-hand" onmouseover="showTip(this)" onmouseleave="hideTip(this)" onclick="displayDesctiption(this)">
+    <area class="body-part" shape="poly" coords="150, 260, 236, 260, 215, 350, 225, 385, 215, 450, 230, 485, 155, 485, 175, 450, 160, 385, 170, 350" alt="legs-area" id="legs-area" onmouseover="showTip(this)" onmouseleave="hideTip(this)" onclick="displayDesctiption(this)">
+```
+  } else if(width > 570 && width < 1000) {
+    map.innerHTML = ```<area class="body-part area" shape="circle" coords="165, 170, 13"  alt="armpit-left" id="armpit-left" onmouseover="showTip(this)" onclick="displayDesctiption(this)" onmouseleave="hideTip(this)">
+    <area class="body-part" shape="circle" coords="220, 170, 13"  alt="armpit-right" id="armpit-right" onmouseover="showTip(this)" onmouseleave="hideTip(this)" onclick="displayDesctiption(this)">
+    <area class="body-part"  shape="poly" coords="150, 40, 185, 30, 210, 25, 230, 70, 240, 95, 250, 105, 240, 110, 180, 115, 170, 110 " alt="face" id="face-area" onclick="displayDesctiption(this)" onmouseover="showTip(this)" onmouseleave="hideTip(this)">
+    <area class="body-part" shape="rect" coords="160, 155, 205, 190" alt="chest" id="chest" onmouseover="showTip(this)" onmouseleave="hideTip(this)" onclick="displayDesctiption(this)">
+    <area class="body-part" shape="rect" coords="175, 245, 205, 275" alt="bikini" id="bikini" onmouseover="showTip(this)" onmouseleave="hideTip(this)" onclick="displayDesctiption(this)">
+    
+    <area class="body-part" shape="poly" coords="170, 185, 215, 185, 223, 200, 230, 245, 155, 245,  165, 200" alt="belly" id="belly" onmouseover="showTip(this)" onmouseleave="hideTip(this)" onclick="displayDesctiption(this)">
+    <area class="body-part" shape="poly" coords="140, 160, 160, 180, 160, 205, 150, 245, 155, 260, 150, 265, 135, 240, 150, 295, 133, 260, 135, 245, 138, 205" alt="left-hand" id="left-hand" onmouseover="showTip(this)" onmouseleave="hideTip(this)" onclick="displayDesctiption(this)">                        
+    <area class="body-part" shape="poly" coords="220, 180, 240, 160, 252, 205, 254, 240, 262, 245,   245, 290, 223, 255, 233, 240, 226, 210" alt="right-hand", id="right-hand" onmouseover="showTip(this)" onmouseleave="hideTip(this)" onclick="displayDesctiption(this)">
+    <area class="body-part" shape="poly" coords="150, 260, 236, 260, 215, 350, 225, 385, 215, 450, 230, 485, 155, 485, 175, 450, 160, 385, 170, 350" alt="legs-area" id="legs-area" onmouseover="showTip(this)" onmouseleave="hideTip(this)" onclick="displayDesctiption(this)">
+```
+  }
+}
